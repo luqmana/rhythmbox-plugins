5d398c645bc9c9cdf243d1ce943ba465a2689600
fossfreedom <fossfreedom@gmail.com>
2014-04-10 12:57:59 +0100

    latest rb3compat module to fix crash - issue #27

--- equalizer/equalizer_rb3compat.py
+++ equalizer/equalizer_rb3compat.py
@@ -29,7 +29,7 @@ from gi.repository import GObject
 from gi.repository import RB
 import sys
 import rb
-import lxml.etree as ET
+import xml.etree.ElementTree as ET
 
 def pygobject_version():
     ''' 
@@ -41,7 +41,27 @@ def pygobject_version():
     str_version = to_number(GObject.pygobject_version)
     
     return float(str_version.rsplit('.',1)[0])
-
+    
+def compare_pygobject_version(version):
+    '''
+    return True if version is less than pygobject_version
+    i.e. 3.9 < 3.11
+    '''
+    to_number = lambda t: ".".join(str(v) for v in t)
+    
+    str_version = to_number(GObject.pygobject_version)
+  
+    split = str_version.rsplit('.',2)
+    split_compare = version.rsplit('.',2)
+    
+    if int(split_compare[0])<int(split[0]):
+        return True
+        
+    if int(split_compare[1])<int(split[1]):
+        return True
+        
+    return False
+        
 PYVER = sys.version_info[0]
 
 if PYVER >= 3:
@@ -133,14 +153,19 @@ def is_rb3(*args):
     else:
         return True 
         
-class Menu(object):
+class Menu(GObject.Object):
     '''
     Menu object used to create window popup menus
     '''
+    __gsignals__ = {
+        'pre-popup': (GObject.SIGNAL_RUN_LAST, None, ())
+        }
+        
     def __init__(self, plugin, shell):
         '''
         Initializes the menu.
         '''
+        super(Menu, self).__init__()
         self.plugin = plugin
         self.shell = shell
         self._unique_num = 0
@@ -353,6 +378,14 @@ class Menu(object):
         else:
             item = self.get_menu_object(menu_or_action_item)
             item.set_sensitive(enable)
+            
+    def popup(self, source, menu_name, button, time):
+        '''
+        utility function to show the popup menu
+        '''
+        self.emit('pre-popup')
+        menu = self.get_gtkmenu(source, menu_name)
+        menu.popup(None, None, None, None, button, time)
             
 class ActionGroup(object):
     '''
