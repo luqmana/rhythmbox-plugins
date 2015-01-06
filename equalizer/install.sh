aa34ada066a001fc434bab912dc4596401167ac3
David Mohammed <fossfreedom@users.noreply.github.com>
2015-01-06 21:09:15 +0000

    use $HOME rather than $USER for those users who's home is not home

--- equalizer/install.sh
+++ equalizer/install.sh
@@ -51,7 +51,7 @@ RB=${RB:=true}
 
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/equalizer/"
+PLUGIN_PATH="${HOME}/.local/share/rhythmbox/plugins/equalizer/"
 #GLIB_SCHEME="org.gnome.rhythmbox.plugins.equalizer.gschema.xml"
 #SCHEMA_FOLDER="schema/"
 #GLIB_DIR="/usr/share/glib-2.0/schemas/"
